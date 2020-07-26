1bd95dbd8fa319d809b95881e56d0db0c900441f
Olivier Van hamme <nicefiction@gmail.com>
2020-07-26 20:22:00 +0200

    Renames ContentView to EmojiMemoryGameView .

--- Memorize/SceneDelegate.swift
+++ Memorize/SceneDelegate.swift
@@ -21,7 +21,7 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
 
         // Create the SwiftUI view that provides the window contents.
         let game = EmojiMemoryGame() // 👋
-        let contentView = ContentView(viewModel : game) // 👋
+        let contentView = EmojiMemoryGameView(viewModel : game) // 👋
 
         // Use a UIHostingController as window root view controller.
         if let windowScene = scene as? UIWindowScene {
--- Memorize/View/EmojiMemoryGameView.swift
+++ Memorize/View/EmojiMemoryGameView.swift
@@ -9,7 +9,7 @@
 import SwiftUI
 
 
-struct ContentView: View {
+struct EmojiMemoryGameView: View {
     
      // /////////////////
     //  MARK: PROPERTIES
@@ -50,7 +50,7 @@ struct ContentView: View {
 struct ContentView_Previews: PreviewProvider {
     
     static var previews: some View {
-        ContentView(viewModel : EmojiMemoryGame())
+        EmojiMemoryGameView(viewModel : EmojiMemoryGame())
     } // static var previews: some View {}
     
 } // struct ContentView_Previews: PreviewProvider {}
